29007c28b4da7eb51114fc3ea3267138d71e2cb2
Pablo Garcia <pablogarciajuanes@gmail.com>
2025-08-03 18:05:12 +0200

    Template listo sin rutas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./shared/header/header";
+import { Footer } from './shared/footer/footer';
+import { Portafolio } from './pages/portafolio/portafolio';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Portafolio],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/shared/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+}
